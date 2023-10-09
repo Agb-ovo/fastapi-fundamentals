d08168534ecfe290d3d5683a52b2c7832e3a25ae
Agb-ovo <agbroko12@gmail.com>
2023-10-09 21:55:25 +0100

    test API

--- carsharing.py
+++ carsharing.py
@@ -1,9 +1,38 @@
 from fastapi import FastAPI
+from datetime import datetime
 
 app = FastAPI()
 
+db = [
+    {"id": 1, "size": "s", "fuel": "gasoline", "doors": 3, "transmission": "auto"},
+    {"id": 2, "size": "s", "fuel": "electric", "doors": 3, "transmission": "auto"},
+    {"id": 3, "size": "s", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+    {"id": 4, "size": "m", "fuel": "electric", "doors": 3, "transmission": "auto"},
+    {"id": 5, "size": "m", "fuel": "hybrid", "doors": 5, "transmission": "auto"},
+    {"id": 6, "size": "m", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+    {"id": 7, "size": "l", "fuel": "diesel", "doors": 5, "transmission": "manual"},
+    {"id": 8, "size": "l", "fuel": "electric", "doors": 5, "transmission": "auto"},
+    {"id": 9, "size": "l", "fuel": "hybrid", "doors": 5, "transmission": "auto"}
+]
+
 @app.get("/")
-def welcome():
-    """"Return a freindly welcome message"""
-    return  {'message': "Welcome to the Car Sharing service"}
+def welcome(name):
+    """"Return a friendly welcome message"""
+    return  {'message': f"Welcome {name} to the Car sharing service"}
+
+
+@app.get("/date")
+def date():
+    """"Return a friendly welcome message"""
+    return {'date': datetime.now()}
+
+@app.get("/api/cars")
+def get_car(size=None):
+    if size:
+        return [car for car in db if car['size'] == size]
+    else:
+        return db
+
+
+
 
