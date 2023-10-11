7e36e19c58682c933a76930d966220025c3d89c7
Agb-ovo <agbroko12@gmail.com>
2023-10-11 12:34:13 +0100

    Replacing database with schema classes(Object oriented data)

--- schemas.py
+++ schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+import json
 
 
 class Car(BaseModel):
@@ -7,3 +8,16 @@ class Car(BaseModel):
     fuel: str | None = "electric"
     doors: int
     transmission: str | None = "auto"
+
+
+#def load_db() -> list[Car]:
+    """"Load a list of Car objects from a JSON file"""
+    #with open("cars.json") as f:
+        #return[Car.model_validate(obj) for obj in json.load(f)]
+
+def load_db() -> list[Car]:
+    with open("cars.json") as f:
+        car_data = json.load(f)
+        return [Car.model_validate(obj) for obj in car_data]
+
+
