edbc55332b6567a9614bdd11298108bb89b5c145
Agb-ovo <agbroko12@gmail.com>
2023-10-28 15:29:59 +0100

    Post Function for Trip Class Utilizing nested Models

--- carsharing.py
+++ carsharing.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from datetime import datetime
 
-from schemas import load_db,CarInput,save_db , CarOutput
+from schemas import load_db, CarInput, save_db, CarOutput, TripOutput, TripInput
 
-app = FastAPI()
+app = FastAPI(title="Car Sharing")
 
 db = load_db()
+
 
 @app.get("/")
 def welcome(name):
@@ -18,9 +19,10 @@ def date():
     """"Return a friendly welcome message"""
     return {'date': datetime.now()}
 
+
 @app.get("/api/cars")
 def get_cars(size: str|None = None, doors: int|None = None) -> list:
-# def get_cars(size: Optional[str] = None, doors: Optional[str] = None) -> List:
+    # def get_cars(size: Optional[str] = None, doors: Optional[str] = None) -> List:
     result = db
     if size:
         result = [car for car in result if car.size == size]
@@ -34,6 +36,7 @@ def car_by_id(id: int):
     result = [car for car in db if car.id == id]
     return result[0]
 
+
 @app.post("/api/cars/", response_model=CarOutput)
 def add_car(car: CarInput) -> CarOutput:
     new_car = CarOutput(size=car.size, doors=car.doors, fuel=car.fuel, transmission=
@@ -41,6 +44,7 @@ def add_car(car: CarInput) -> CarOutput:
     db.append(new_car)
     save_db(db)
     return new_car
+
 
 @app.delete("/api/cars/{id}", status_code=204)
 def remove_car(id: int) -> None:
@@ -51,6 +55,7 @@ def remove_car(id: int) -> None:
         save_db(db)
     else:
         raise HTTPException(status_code=404, detail=f"No car with id={id}.")
+
 
 @app.put("/api/cars/{id}", response_model=CarOutput)
 def change_car(id: int, new_data: CarInput) -> CarOutput:
@@ -65,6 +70,23 @@ def change_car(id: int, new_data: CarInput) -> CarOutput:
         return car
     else:
         raise HTTPException(status_code=404, detail=f"No car with id={id}.")
+
+
+@app.post("/api/cars/{car_id}/trips", response_model=TripOutput)
+def add_trip(car_id: int, trip: TripInput) -> TripOutput:
+    matches = [car for car in db if car.id == car_id]
+    if matches:
+        car = matches[0]
+        new_trip = TripOutput(id=len(car.trips)+1,
+                              start=trip.start, end=trip.end,
+                              description=trip.description)
+        car.trips.append(new_trip)
+        save_db(db)
+        return new_trip
+    else:
+        raise HTTPException(status_code=404, detail=f"No car with id={id}.")
+
+
 
 
 
--- schemas.py
+++ schemas.py
@@ -28,19 +28,22 @@ class CarInput(BaseModel):
             }
         }
 
+
 class CarOutput(CarInput):
     id: int
     trips: list[TripOutput] = []
 
-#def load_db() -> list[Car]:
+     #def load_db() -> list[Car]:
     """"Load a list of Car objects from a JSON file"""
-    #with open("cars.json") as f:
+        #with open("cars.json") as f:
         #return[Car.model_validate(obj) for obj in json.load(f)]
+
 
 def load_db() -> list[CarOutput]:
     with open("cars.json") as f:
         car_data = json.load(f)
         return [CarOutput.model_validate(obj) for obj in car_data]
+
 
 def save_db(cars: list[CarInput]):
     with open("cars.json", 'w') as f:
